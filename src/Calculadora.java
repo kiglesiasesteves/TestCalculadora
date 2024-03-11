f6dcf86688c62c25215f30f52e9555df8b5c064a
Keyla Iglesias <kiglesiasesteves@danielcastelao.org>
2024-03-11 09:21:46 +0100

    Merge Squash

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -3,63 +3,154 @@ import com.DAM2024.librerias.Salida;
 
 import javax.swing.*;
 
-
-public class Calculadora{
+/**
+ * Esta clase proporciona métodos para realizar operaciones matemáticas básicas, como suma, resta, multiplicación, división y raíz cuadrada.
+ */
+public class Calculadora {
+    /**
+     * Constante para la operación de suma.
+     */
     public final static int SUMA = 1;
-    public final static int MULTIPLICACION = 3;
-
-    public final static int DIVISION = 4;
-
+    /**
+     * Constante para la operación de resta.
+     */
     public final static int RESTA = 2;
+    /**
+     * Constante para la operación de multiplicación.
+     */
+    public final static int MULTIPLICACION = 3;
+    /**
+     * Constante para la operación de división.
+     */
+    public final static int DIVISION = 4;
+    /**
+     * Constante para la operación de raíz cuadrada.
+     */
+    public final static int RAIZ = 5;
 
-
+    /**
+     * Realiza la operación matemática especificada con los dos datos dados.
+     *
+     * @param dato1 El primer número para la operación.
+     * @param dato2 El segundo número para la operación.
+     * @return El resultado de la operación matemática.
+     */
     public static Float Calculadora(float dato1, float dato2) {
-        /**
-         * @param float dato1
-         * @param float dato2
-         *
-         * @return número entero
-         *
-         */
+        Float resultado = 0.0f;
 
-        Float resultado=0.0f;
+        int opcion = getOpcion();
+        String mensaje = getMensaje(opcion);
 
-        int opcion= Integer.parseInt(JOptionPane.showInputDialog("Qué operación queres realizar \n 1. Suma \n 2. Multiplicación \n 3. Resta \n 4. División\""));
-        String mensaje=null;
+        resultado = switch (opcion) {
+            case SUMA -> getSuma(dato1, dato2);
+            case MULTIPLICACION -> getMultiplicacion(dato1, dato2);
+            case RESTA -> getResta(dato1, dato2);
+            case DIVISION -> getDivision(dato1, dato2);
+            case RAIZ -> getRaiz(dato1, dato2);
+            default -> null;
+        };
 
-        switch (opcion) {
-            case SUMA:
-                resultado = dato1 + dato2;
-                mensaje="Suma";
-                break;
-            case MULTIPLICACION:
-                resultado = dato1 * dato2;
-                mensaje="Multiplication";
-
-                break;
-            case RESTA:
-                resultado = dato1 - dato2;
-                mensaje="Resta";
-
-                break;
-            case DIVISION:
-            try {
-                resultado = dato1 / dato2;
-            }catch (ArithmeticException e){
-                JOptionPane.showMessageDialog(null,"No se puede dividir por cero");
-            }finally{
-                break;
-            }
-
-            default:
-
-                resultado = null;
-        }
-
-        JOptionPane.showMessageDialog(null,"EL resultado entero de "+mensaje+" con los datos "+dato1+ " y " +dato2 +" es: ");
-       return resultado;
-
+        JOptionPane.showMessageDialog(null, "El resultado de " + getMensaje(opcion) + " con los datos " + dato1 + " y " + dato2 + " es: ");
+        return resultado;
     }
 
+    /**
+     * Calcula la raíz cuadrada del segundo dato elevado a la inversa del primer dato.
+     *
+     * @param dato1 El primer número para la operación.
+     * @param dato2 El segundo número para la operación.
+     * @return La raíz cuadrada del segundo dato elevado a la inversa del primer dato.
+     */
+    private static Float getRaiz(float dato1, float dato2) {
+        Float resultado;
+        if (dato1 == 0) {
+            resultado = 1F;
+        } else {
+            resultado = (float) Math.pow(dato2, 1 / dato1);
+        }
+        return resultado;
+    }
 
+    /**
+     * Realiza la división de los dos datos.
+     *
+     * @param dato1 El dividendo.
+     * @param dato2 El divisor.
+     * @return El resultado de la división.
+     */
+    private static float getDivision(float dato1, float dato2) {
+        try {
+            return dato1 / dato2;
+        } catch (ArithmeticException e) {
+            JOptionPane.showMessageDialog(null, "No se puede dividir por cero");
+        }
+        return dato1 / dato2;
+    }
+
+    /**
+     * Realiza la resta de los dos datos.
+     *
+     * @param dato1 El primer número.
+     * @param dato2 El segundo número.
+     * @return El resultado de la resta.
+     */
+    private static Float getResta(float dato1, float dato2) {
+        Float resultado;
+        resultado = dato1 - dato2;
+        return resultado;
+    }
+
+    /**
+     * Realiza la multiplicación de los dos datos.
+     *
+     * @param dato1 El primer número.
+     * @param dato2 El segundo número.
+     * @return El resultado de la multiplicación.
+     */
+    private static Float getMultiplicacion(float dato1, float dato2) {
+        Float resultado;
+        resultado = dato1 * dato2;
+        return resultado;
+    }
+
+    /**
+     * Realiza la suma de los dos datos.
+     *
+     * @param dato1 El primer número.
+     * @param dato2 El segundo número.
+     * @return El resultado de la suma.
+     */
+    private static Float getSuma(float dato1, float dato2) {
+        Float resultado;
+        resultado = dato1 + dato2;
+        return resultado;
+    }
+
+    /**
+     * Devuelve el mensaje correspondiente a la opción seleccionada.
+     *
+     * @param opcion La opción seleccionada.
+     * @return El mensaje correspondiente a la opción seleccionada.
+     */
+    private static String getMensaje(int opcion) {
+        String mensaje = switch (opcion) {
+            case SUMA -> "Suma";
+            case MULTIPLICACION -> "Multiplicación";
+            case RESTA -> "Resta";
+            case DIVISION -> "División";
+            case RAIZ -> "Raíz";
+            default -> null;
+        };
+        return mensaje;
+    }
+
+    /**
+     * Solicita al usuario que ingrese la opción deseada.
+     *
+     * @return La opción seleccionada por el usuario.
+     */
+    static int getOpcion() {
+        int opcion = Integer.parseInt(JOptionPane.showInputDialog("Qué operación quieres realizar?\n 1. Suma\n 2. Multiplicación\n 3. Resta\n 4. División\n 5. Raíz"));
+        return opcion;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,35 @@ import com.DAM2024.librerias.Salida;
 
 import javax.swing.*;
 
+/**
+ * La clase Main contiene el método principal para ejecutar el programa.
+ * Toma la entrada del usuario a través de la consola o ventana, realiza cálculos usando la clase Calculadora,
+ * y muestra el resultado usando la clase Salida.
+ */
 public class Main {
-        public static void main(String[] args) {
 
-               Float retorno = Calculadora.Calculadora(Entrada.entrada_ventana_consola_float("Tecle el primer numero",Entrada.VENTANA),Entrada.entrada_ventana_consola_float("Tecle el segundo numero",Entrada.VENTANA));
+    /**
+     * El método principal del programa.
+     * Solicita al usuario que ingrese dos números, realiza cálculos usando la clase Calculadora,
+     * y muestra el resultado o un mensaje de error usando la clase Salida.
+     *
+     * @param args Los argumentos de la línea de comandos (no se utilizan en este programa).
+     */
+    public static void main(String[] args) {
 
-               //Comprobacion del retorno
-               if(retorno == null){
-                   Salida.salida_ventana_consola("Algo fallo",Salida.CONSOLA);
-               }else{
-                   Salida.salida_float("Resultado: ",retorno,Salida.CONSOLA);
-               }
-           }
-       }
+        // Solicita al usuario que ingrese dos números
+        Float retorno = Calculadora.Calculadora(
+                Entrada.entrada_ventana_consola_float("Tecle el primer número (en caso de querer realizar una raíz, este será el radicando)", Entrada.VENTANA),
+                Entrada.entrada_ventana_consola_float("Tecle el segundo número (en caso de querer realizar una raíz, este será el índice)", Entrada.VENTANA));
 
+        // Verifica el valor de retorno y muestra el resultado o un mensaje de error
+        if (retorno == null) {
+            Salida.salida_ventana_consola("Algo falló", Salida.CONSOLA);
+        } else {
+            Salida.salida_float("Resultado: ", retorno, Salida.CONSOLA);
+        }
+    }
+}
 
     
 
